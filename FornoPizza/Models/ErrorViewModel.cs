9b78dc5eef0d1876efa72089fba583efcdbeb7a5
Денис Кибицкий <aidns410@gmail.com>
2026-07-16 20:52:31 +0300

    Initial commit: ASP.NET FornoPizza project

---
+++ FornoPizza/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FornoPizza.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
